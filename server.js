f9ec109eaf085960a24f179291f3499a18b56d92
millywanderi <wanderinyambura5@gmail.com>
2024-03-11 10:46:28 +0300

    app with routes and controllers

--- server.js
+++ server.js
@@ -1,15 +1,12 @@
 import express from 'express';
-import router from './routes/index';
-
-const port = parseInt(process.env.PORT, 10) || 5000;
+import routes from './routes';
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use('/', router);
+app.use(routes);
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
 });
-
-export default app;
